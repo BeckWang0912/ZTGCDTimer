c7bd9b1cea6bd59faa84da873e251b41c221fc0b
beck.wang <beck.wang@fmchexian.com>
2017-06-16 16:10:48 +0800

    Initial Commit

---
+++ ZTGCDTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZTGCDTimer
+//
+//  Created by BY-iMac on 17/6/16.
+//  Copyright © 2017年 beck.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZTGCDTimerTests/ZTGCDTimerTests.m
@@ -0,0 +1,39 @@
+//
+//  ZTGCDTimerTests.m
+//  ZTGCDTimerTests
+//
+//  Created by BY-iMac on 17/6/16.
+//  Copyright © 2017年 beck.wang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZTGCDTimerTests : XCTestCase
+
+@end
+
+@implementation ZTGCDTimerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
